e69ca1d7496afba8b2af2e7c66f257f2917e07d2
Poltorak Serguei <ps@ps>
2015-12-29 21:39:02 +0300

    Improvements from Alex and fixed whitespaces.

--- uSAPI.c
+++ uSAPI.c
@@ -18,7 +18,6 @@
 #define REQUEST_CODE    0x00
 #define RESPONSE_CODE   0x01
 
-
 #define RETCODE_OK                  0
 #define RETCODE_NOACK               1
 #define RETCODE_INVALID_DATALEN     2
@@ -27,9 +26,7 @@
 #define RETCODE_INVALID_SOF         5
 #define RETCODE_INVALID_SOF_SIZE    6
 
-
-
-#define OUTPUT_BUFFER_SIZE          512    
+#define OUTPUT_BUFFER_SIZE          512
 
 #define ADDITIONAL_SIZE             2
 #define ADDITIONAL_SIZE_SL          2
@@ -38,18 +35,16 @@
 
 static int debug_flag = 0;
 static int callback_flag = 0;
+static int max_timeout   = 3;
 
 typedef unsigned char BYTE;
-
 
 BYTE g_output_buffer[OUTPUT_BUFFER_SIZE];
 int           g_seq_number    =     1;
 
-
-
 int zio_open(char * name)
 {
-   return open(name, O_RDWR | O_NOCTTY);    
+   return open(name, O_RDWR | O_NOCTTY);
 }
 
 void zio_close(int dev)
@@ -59,15 +54,13 @@ void zio_close(int dev)
 
 void zio_configure(int dev, int baudRate)
 {
-
-    struct termios cfg;        
+    struct termios cfg;
     memset(&cfg, 0, sizeof(cfg));
     
     tcgetattr(dev, &cfg);
 
     cfsetospeed(&cfg, baudRate);
     cfsetispeed(&cfg, baudRate);
-    
     
     cfg.c_cflag     &=  ~PARENB;            // Make 8n1
     cfg.c_cflag     &=  ~CSTOPB;
@@ -80,14 +73,11 @@ void zio_configure(int dev, int baudRate)
 
     cfg.c_cflag     |=  CREAD | CLOCAL;
 
-    
-    
     cfmakeraw(&cfg);
 
     //  Flush Port, then applies attributes 
     tcflush( dev, TCIFLUSH );
 
-        
     if (0 != tcsetattr(dev, TCSANOW, &cfg))
     {
         fprintf (stderr, "Failed to alter device settings\n");
@@ -102,7 +92,7 @@ int zio_write(int dev, BYTE *buffer, BYTE size)
     
     if (result <= 0)
     {
-        fprintf (stderr, "IO Error: write returns %d\n", result);   
+        fprintf (stderr, "IO Error: write returns %d\n", result);
         return -1;
     }
     
@@ -113,32 +103,32 @@ void printPacket(BYTE * buff, int data_len)
 {
     int i;
 
-    for(i=0;i<data_len;i++)
+    for (i=0; i<data_len; i++)
     {
-        if(i!=0)
+        if (i!=0)
             fprintf(stderr, ",");
         fprintf(stderr,"%x",buff[i]);
 
     }
 }
+
 int zio_read(int dev, BYTE *buffer, BYTE size)
 {
     int result;
     int offset = 0;
-    long end_time =  (unsigned)time(NULL) + 5; 
-
+    long end_time =  (unsigned)time(NULL) + max_timeout;
    
     fd_set set;
     struct timeval tv; // 10ms
     
-    while(offset < size)
+    while (offset < size)
     {
         FD_ZERO(&set);
         FD_SET(dev, &set);
     
-        tv.tv_sec = 2;
+        tv.tv_sec = 0;
         tv.tv_usec = 100*1000;
-        if(((unsigned)time(NULL)) >  end_time)
+        if (((unsigned)time(NULL)) >  end_time)
             break;
         result = select(dev+1, &set, NULL, NULL, &tv);
         if (result < 0)
@@ -146,12 +136,12 @@ int zio_read(int dev, BYTE *buffer, BYTE size)
             fprintf (stderr, "IO Error: select fails for device. Result: %d\n", result);
             return 0;
         }
-        if(result != 0)
+        if (result != 0)
         {
             result = (int)read(dev, buffer + offset, size - offset);
         
             if (result < 0)
-            {   
+            {
                 fprintf (stderr, "IO Error: read fails for device. Result: %d\n", result);
                 return -1;
             }
@@ -162,45 +152,42 @@ int zio_read(int dev, BYTE *buffer, BYTE size)
     return offset;
 }
 
-
-
 BYTE crc8_SD(BYTE * buff, int data_len, int offset)
 {
     int i;
     BYTE ret = 0xff;
 
-    for(i=0;i<data_len;i++)
+    for (i=0; i<data_len; i++)
     {
         ret = ret ^ buff[offset + i];
         
     }
-       
+    
     return ret;
 }
 
 void sendAckNack(int fd, BYTE code)
 {
-        g_output_buffer[0] = code;
-        zio_write(fd, g_output_buffer, 1);
+    g_output_buffer[0] = code;
+    zio_write(fd, g_output_buffer, 1);
 
-        if(debug_flag)
-        {
-            if(code == ACK_CODE)
-                fprintf(stderr, ">> ACK\n");
-            else
-                fprintf(stderr, ">> NACK\n");
-        }    
+    if (debug_flag)
+    {
+        if (code == ACK_CODE)
+            fprintf(stderr, ">> ACK\n");
+        else
+            fprintf(stderr, ">> NACK\n");
+    }
 }
 
 void send_SAPIData(int fd, BYTE * buff, int data_len, bool bHaveCallback)
 {
     int packet_len = data_len + ADDITIONAL_SIZE;
 
-    if(bHaveCallback)
+    if (bHaveCallback)
     {
         g_output_buffer[data_len + ADDITIONAL_HEADSIZE] =   (g_seq_number++) & 0xFF;
         packet_len++;
-        
     }
 
     g_output_buffer[0] = SOF_CODE;
@@ -211,16 +198,15 @@ void send_SAPIData(int fd, BYTE * buff, int data_len, bool bHaveCallback)
 
     g_output_buffer[packet_len + 1] = crc8_SD(g_output_buffer, packet_len, 1);
 
-    if(debug_flag)
+    if (debug_flag)
     {
         fprintf(stderr,"sending >> ");
         printPacket(g_output_buffer, packet_len + 2);
         fprintf(stderr,"\n");
     }
     zio_write(fd, g_output_buffer, packet_len + 2);
-
-    
 }
+
 int receive_SAPIData(int fd, BYTE * receive_buff, BYTE * receive_len)
 {
     int bytes_readed = 0;
@@ -229,32 +215,32 @@ int receive_SAPIData(int fd, BYTE * receive_buff, BYTE * receive_len)
 
     bytes_readed = zio_read(fd, receive_buff, 2);
 
-    if(bytes_readed != 2)
+    if (bytes_readed != 2)
     {
         return RETCODE_INVALID_SOF_SIZE;
     }
 
-    if(receive_buff[0] != SOF_CODE)
+    if (receive_buff[0] != SOF_CODE)
         return RETCODE_INVALID_SOF;
 
     packet_len = receive_buff[1];
     bytes_readed = zio_read(fd, receive_buff + 2, packet_len);
 
-    if(debug_flag)
+    if (debug_flag)
     {
         fprintf(stderr,"received << ");
         printPacket(receive_buff, bytes_readed + 2);//packet_len + 2);
         fprintf(stderr,"\n");
     }
 
-    if(bytes_readed != packet_len)
+    if (bytes_readed != packet_len)
     {
-        fprintf(stderr,"received << ");   
+        fprintf(stderr,"received << ");
         sendAckNack(fd, NACK_CODE);
         return RETCODE_INVALID_DATALEN;
     }
 
-    if(crc8_SD(receive_buff, packet_len, 1) != receive_buff[2 + packet_len - 1])
+    if (crc8_SD(receive_buff, packet_len, 1) != receive_buff[2 + packet_len - 1])
     {
 
         sendAckNack(fd, NACK_CODE);
@@ -266,7 +252,6 @@ int receive_SAPIData(int fd, BYTE * receive_buff, BYTE * receive_len)
     sendAckNack(fd, ACK_CODE);
 
     return RETCODE_OK;
-
 }
 
 int send_SAPICommand(int fd, BYTE * cmd_buff, int data_len, BYTE * cmd_response, BYTE * response_len, bool bcallback)
@@ -275,19 +260,30 @@ int send_SAPICommand(int fd, BYTE * cmd_buff, int data_len, BYTE * cmd_response,
     int packet_len;
     BYTE ack;
 
-    
-    send_SAPIData(fd, cmd_buff, data_len, bcallback);
 
-    bytes_readed = zio_read(fd, &ack, 1);
-    if((bytes_readed != 1) || (ack != ACK_CODE))
-        return RETCODE_NOACK;
+    if (data_len > 0)
+    {
+        send_SAPIData(fd, cmd_buff, data_len, bcallback);
 
-    if(cmd_response == NULL)
-        return RETCODE_OK;
+        bytes_readed = zio_read(fd, &ack, 1);
+        if ((bytes_readed != 1) || (ack != ACK_CODE))
+        {
+            if (debug_flag)
+            {
+                fprintf(stderr,"%x instead ACK \n", ack);
+            }
+            return RETCODE_NOACK;
+        }
+        if (debug_flag)
+        {
+            fprintf(stderr,"<< ACK \n");
+        }
+
+         if (cmd_response == NULL)
+            return RETCODE_OK;
+    }
 
     return receive_SAPIData(fd, cmd_response, response_len);
-
-
 }   
 
 bool isHexDigit(char digit)
@@ -297,23 +293,28 @@ bool isHexDigit(char digit)
 
 BYTE hexCharToByte(char digit)
 {
-    if(('A' <= digit && digit <= 'F'))
+    if (('A' <= digit && digit <= 'F'))
         return (digit - 'A' + 10);
     return ('0' <= digit && digit <= '9') ? (digit - '0') : (digit - 'a' + 10);
 }
+
 int hexStrToBA(char * hexStr, BYTE * val)
 {
     BYTE count = 0;
     int i = 0;
     
-    while (i < strlen(hexStr)) {
-        if (hexStr[i] == ' ') {
+    while (i < strlen(hexStr))
+    {
+        if (hexStr[i] == ' ')
+        {
             i++;
             continue;
         }
         
-        if (isHexDigit(hexStr[i])) {
-            if ((i + 1) < strlen(hexStr) && isHexDigit(hexStr[i+1])) {
+        if (isHexDigit(hexStr[i]))
+        {
+            if ((i + 1) < strlen(hexStr) && isHexDigit(hexStr[i+1]))
+            {
                 if ((i + 2) < strlen(hexStr) && hexStr[i+2] != ' ')
                     printf("paring hex array (three subsequent digits): %s\n", hexStr);
                 val[count++] = hexCharToByte(hexStr[i]) * 16 + hexCharToByte(hexStr[i+1]);
@@ -328,14 +329,16 @@ int hexStrToBA(char * hexStr, BYTE * val)
     }
     return count;
 }
+
 static struct option long_options[] =
 {
     {"buffer", required_argument, NULL, 'b'},
     {"port", required_argument, NULL, 'p'},
-    {"responce_counter", required_argument,  NULL, 'r'}, 
+    {"responce_counter", required_argument,  NULL, 'r'},
+    {"timeout", required_argument, NULL, 't'},
 
-    {"debug", no_argument,       &debug_flag, 1}, 
-    {"callback", no_argument,       &callback_flag, 1}, 
+    {"debug", no_argument,       &debug_flag, 1},
+    {"callback", no_argument,       &callback_flag, 1},
     
     {NULL, 0, NULL, 0}
 };
@@ -347,23 +350,23 @@ void printResponse(BYTE * buff,int num, int data_len)
 {
     int i;
 
-    printf("RESP%d:{",num);
-    for(i=0;i<data_len;i++)
+    printf("RESP %d {",num);
+    for (i=0; i<data_len; i++)
     {
-        if(i!=0)
-            fprintf(stdout, " ");
-        fprintf(stdout,"%x",buff[i]);
+        //if (i!=0)
+        // fprintf(stdout, " ");
+        fprintf(stdout," %02x",buff[i]);
 
     }
-    fprintf(stdout,"}\n");
+    fprintf(stdout," }\n");
 }
+
 void printUsage()
 {
-     fprintf(stderr, "Wrong usage of this utility.\n Right format is: uSAPI -b <byte_array> -p <devname> [-r <number_of_waiting responces>] [--callback] [--debug]\n");
-               
+     fprintf(stderr, "Wrong usage of this utility.\n Right format is: uSAPI -b <byte_array> -p <devname> [-r <number_of_waiting responces>] [--callback] [--debug]\n");               
 }
-int
-main (int argc, char **argv)
+
+int main (int argc, char **argv)
 {
     char *portname;
 
@@ -378,18 +381,19 @@ main (int argc, char **argv)
     static int opt;
     static int longIndex;
 
-    if(argc < 3)
+    if (argc < 3)
     {
         printUsage();
         exit(-100);
     }
     
-    while( 1 ) {
-        opt = getopt_long( argc, argv, "b:p:r:", long_options, NULL);
-        if(opt == -1)
+    while (1)
+    {
+        opt = getopt_long( argc, argv, "b:p:r:t:", long_options, NULL);
+        if (opt == -1)
             break;
-        switch( opt ) {
-                
+        switch( opt )
+        {
             case 'b':
                 out_data_len = hexStrToBA(optarg, out_rawdata);
                 break;
@@ -402,11 +406,14 @@ main (int argc, char **argv)
                 num_responces_we_need = atoi(optarg);
                 break;
                 
-                
-             case 0:     /* длинная опция без короткого эквивалента */
+            case 't':
+                max_timeout = atoi(optarg);
                 break;
 
-             case '?':
+            case 0:     /* длинная опция без короткого эквивалента */
+                break;
+
+            case '?':
                 printUsage();
                  /* getopt_long already printed an error message. */
                 exit(-100); 
@@ -416,19 +423,17 @@ main (int argc, char **argv)
                 /* сюда попасть невозможно. */
                 break;
         }
-        
     }
-
     
     fd = zio_open(portname);
     if (fd < 0)
-    {   
+    {
         fprintf(stderr,"error %d opening %s: %s\n", errno, portname, strerror (errno));
         return -10;
     }
-    zio_configure(fd, B115200); 
+    zio_configure(fd, B115200);
 
-    if(num_responces_we_need == 0)
+    if (num_responces_we_need == 0)
     {
         ret_code = send_SAPICommand(fd, out_rawdata, out_data_len,  NULL, NULL, 0);
         zio_close(fd);
@@ -438,7 +443,7 @@ main (int argc, char **argv)
     {
         BYTE resp_counter = 1;
         ret_code = send_SAPICommand(fd, out_rawdata, out_data_len,  in_rawdata, &in_data_len, callback_flag);
-        if(ret_code != 0)
+        if (ret_code != 0)
         {
             zio_close(fd);
             exit(ret_code);
@@ -447,10 +452,10 @@ main (int argc, char **argv)
 
         num_responces_we_need --;
 
-        while(num_responces_we_need)
+        while (num_responces_we_need)
         {
             ret_code = receive_SAPIData(fd,  in_rawdata, &in_data_len);
-            if(ret_code != 0)
+            if (ret_code != 0)
             {
                 zio_close(fd);
                 exit(ret_code);
